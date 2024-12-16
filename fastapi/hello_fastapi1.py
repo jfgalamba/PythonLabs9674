4d3ccc68b7a8b10eb763f775ea16bb3c6bca8203
Joao Galamba <jfgmmg@Joaos-MacBook-Pro.local>
2024-12-16 17:06:52 +0000

    hello_fastapi1 completed

--- fastapi/hello_fastapi1.py
+++ fastapi/hello_fastapi1.py
@@ -24,5 +24,35 @@ async def index():
 
 @app.get('/computeitem/{value}')
 async def compute_item(value: int, param1: str | None = None, param2: str | None = None):
+    # raise ValueError('Big problema!!!')
     return {'2 x value': 2 * value, 'text': f'{param1}/{param2}'}
 #:
+
+
+"""
+Criámos uma api API que:
+
+o Recebe pedidos HTTP nos caminhos / e /computeitem/{value}.
+o Ambos os caminhos aceitam pedidos HTTP GET
+o O caminho /computeitem/{value} tem um parâmetro de caminho value
+  que deve ser um int.
+o O caminho /computeitem/{value} tem dois parâmetros de consulta str 
+  (query string) opcionais param1 e param2.
+
+Agora aceda a: http://127.0.0.1:8000/docs
+Agora aceda a: http://127.0.0.1:8000/redoc
+"""
+
+# EXERCÍCIO: 
+#       1. Correr no vosso repositório o hello_fastapi1.py
+#       2. Testar
+#       3. Acrescentar função para receber dois parâmetros x e y e devolver
+#          a soma de x com y. A função deve ser acessível por get e o caminho
+#          deve começar com "/sum". A função deve-se chamar "sum".
+#
+
+@app.get("/sum/{x}/{y}")
+async def sum(x: int, y: int):
+    return {"x + y": x + y}
+#:
+
